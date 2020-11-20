609dbe6eb66ba323dba8eb7c298932d9aa5f3f98
prekidac <al.slavisa@gmail.com>
2020-11-20 23:32:50 +0100

    done

--- vreme.py
+++ vreme.py
@@ -2,16 +2,18 @@
 
 import argparse, bs4, requests
 import logging
+from colored import fg, attr
 
 logging.basicConfig(level=logging.DEBUG, filename="o.log")
-link = "http://www.hidmet.gov.rs/latin/prognoza/index.php" 
+link = "http://www.hidmet.gov.rs/latin/osmotreni/index.php" 
 
 
 class Vreme(object):
     
-    def __init__(self, city: str = "Novi sad"):
+    def __init__(self, city: str = "Novi_sad"):
         self.load_page()
         self.parse()
+        self.all_cities()
         self.city = city
 
     def load_page(self) -> None:
@@ -22,22 +24,31 @@ class Vreme(object):
         soup_obj = bs4.BeautifulSoup(self.html, features="html.parser")
         self.rows = soup_obj.select("tr")
     
-    def all_cities(self) -> str:
-        cities = []
-        for row in self.rows[3:-3]:
+    def all_cities(self) -> None:
+        self.cities = []
+        self.temps = []
+        for row in self.rows[5:-4]:
             soup_obj = bs4.BeautifulSoup(str(row), features="html.parser")
             city = soup_obj.select("td")
-            cities.append(city[0].getText().replace(u"\xa0", u"").replace(" ", "_"))
-        return ' '.join(cities)
+            self.cities.append(city[0].getText().replace(u"\xa0", u"").replace(" ", "_"))
+            self.temps.append(city[1].getText().replace(u"\xa0", u"").strip())
+        return 
+    
+    def city_temp_print(self) -> None:
+        if self.city in self.cities:
+            self.temp = self.temps[self.cities.index(self.city)]
+            print(f"{fg(3) + self.city + attr('reset')}: {fg(2) + attr(1) + self.temp + attr('reset')} oC")
+        else:
+            exit('Pogresan unos')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trenutno vreme sa hidmet.gov.rs")
     parser.add_argument("-c", action="store_true", help="list of all cities")
-    parser.add_argument("city", action="store", nargs="?", default="Novi sad")
+    parser.add_argument("city", action="store", nargs="?", default="Novi_Sad")
     args = parser.parse_args()
     v = Vreme(args.city)
     if args.c:
-        print(v.all_cities())
+        print(' '.join(v.cities))
     else:
-        print(args.city)
+        v.city_temp_print()
