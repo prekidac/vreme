0583629484c23c3ad18a5ce261cf540f5bf1992a
prekidac <al.slavisa@gmail.com>
2020-11-20 23:32:43 +0100

    completion hash

--- vreme.sh
+++ vreme.sh
@@ -2,7 +2,12 @@ _vreme() {
    local cur opts
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-    opts="Novi_sad Beograd"
+	if [[ -f /tmp/vreme ]]; then
+		opts=$(</tmp/vreme)
+	else
+		opts="$(vreme -c)"
+		echo >/tmp/vreme "${opts}"
+	fi
 	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
 }
 complete -F _vreme vreme 
