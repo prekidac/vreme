45d96617c6bccad7ffc180861f846fe6a8099953
prekidac <al.slavisa@gmail.com>
2020-11-20 19:57:42 +0100

    logging added

--- vreme.py
+++ vreme.py
@@ -1,15 +1,25 @@
 import argparse, bs4, requests
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="o.log")
+link = "http://www.hidmet.gov.rs/latin/prognoza/index.php" 
 
 
 class Vreme(object):
     
     def __init__(self):
         self.load_page()
+        self.parse()
 
     def load_page(self):
-        res_obj = requests.get("http://www.hidmet.gov.rs")
-        exit(res_obj.text)
-        soup_obj = bs4.BeautifulSoup()
+        res_obj = requests.get(link)
+        self.html = res_obj.text
+        logging.debug(self.html)
+
+    def parse(self):
+        soup_obj = bs4.BeautifulSoup(self.html, features="html.parser")
+        self.rows = soup_obj.select("tr")
+        exit(self.rows[0].getText())
 
 
 if __name__ == "__main__":
