51687d93fa64a49cf51f0933e48b06f5adf6e3e9
prekidac <al.slavisa@gmail.com>
2020-11-21 08:21:38 +0100

    multiple cities

--- vreme.py
+++ vreme.py
@@ -12,7 +12,7 @@ class Vreme(object):
         self.load_page()
         self.parse()
         self.all_cities()
-        self.city = city
+        self.cities = city
 
     def load_page(self) -> None:
         res_obj = requests.get(link)
@@ -23,34 +23,40 @@ class Vreme(object):
         self.rows = soup_obj.select("tr")
     
     def all_cities(self) -> None:
-        self.cities = []
-        self.temps = []
+        self.lst_all_cities = []
+        self.lst_all_temps = []
         for row in self.rows[5:-4]:
             soup_obj = bs4.BeautifulSoup(str(row), features="html.parser")
             city = soup_obj.select("td")
-            self.cities.append(city[0].getText().replace(u"\xa0", u"").replace(" ", "_"))
+            self.lst_all_cities.append(city[0].getText().replace(u"\xa0", u"").replace(" ", "_"))
             try: 
-                self.temps.append(round(float(city[1].getText().replace(u"\xa0", u"").strip())))
+                self.lst_all_temps.append(round(float(city[1].getText().replace(u"\xa0", u"").strip())))
             except:
-                self.temps.append(round(float(city[2].getText().replace(u"\xa0", u"").strip())))
+                self.lst_all_temps.append(round(float(city[2].getText().replace(u"\xa0", u"").strip())))
 
-        return 
+        return(' '.join(self.lst_all_cities))
     
-    def city_temp_print(self) -> None:
-        if self.city in self.cities:
-            self.temp = self.temps[self.cities.index(self.city)]
-            print(f"{fg(8) + self.city + attr('reset')}: {fg(2) + attr(1) + str(self.temp) + attr('reset')} oC")
-        else:
-            exit('Pogresan unos')
+    def cities_temp_print(self) -> None:
+        MAX = 0
+        for i in self.cities:
+            if len(i) > MAX: MAX = len(i)
+        for city in self.cities:
+            if city in self.lst_all_cities:
+                self.temp = self.lst_all_temps[self.lst_all_cities.index(city)]
+                print(f"""{fg(8) + city.ljust(MAX) + attr('reset')} {fg(2) + attr(1) + 
+                    str(self.temp) + attr('reset')} {fg(8)}oC{attr('reset')}""")
+            else:
+                WRONG="--"
+                print(f"{fg(2) + attr(1)} {WRONG.center(10)} {attr('reset')} ")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trenutno vreme sa hidmet.gov.rs")
     parser.add_argument("-c", action="store_true", help="list of all cities")
-    parser.add_argument("city", action="store", nargs="?", default="Novi_Sad")
+    parser.add_argument("city", action="store", nargs="*", default=["Novi_Sad"])
     args = parser.parse_args()
     v = Vreme(args.city)
     if args.c:
-        print(' '.join(v.cities))
+        print(v.all_cities())
     else:
-        v.city_temp_print()
+        v.cities_temp_print()
